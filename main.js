4349e3485fe81b9287529fa97933d91c99d5057f
Kanika Wadhawan <wadhawan.kanika@gmail.com>
2019-07-11 16:43:07 -0700

    worked on the flickity slider in the most-loved section

--- main.js
+++ main.js
@@ -1,19 +1,26 @@
-document.addEventListener("DOMContentLoaded", function() {
-    // all of your JS code goes here
+document.addEventListener("DOMContentLoaded", function () {
+  // all of your JS code goes here
 
 
-    let inputEmail= document.getElementById('signup-form');
+  let inputEmail = document.getElementById('signup-form');
 
-    inputEmail.addEventListener("submit",function(e)
-    {
+  inputEmail.addEventListener("submit", function (e) {
     e.preventDefault();
-        
-    const t=document.getElementById("input");
-    t.value.length?(alert("Thanks for subscribing!"),
-    t.value=""):alert("Please submit a valid email address.")
-    
 
-    });
- 
+    const t = document.getElementById("input");
+    t.value.length ? (alert("Thanks for subscribing!"),
+      t.value = "") : alert("Please submit a valid email address.")
 
   });
+
+
+    // Flickity carousel 
+    const elem = document.querySelector('.main-carousel');
+    const flkty = new Flickity(elem, {
+      // options
+      cellAlign: 'left',
+      wrapAround: true,
+      freeScroll: true
+    });
+
+});
