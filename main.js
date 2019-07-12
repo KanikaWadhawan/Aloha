43af7856882e8769c8a830dfc9f5f7a3c98fb460
Kanika Wadhawan <wadhawan.kanika@gmail.com>
2019-07-12 13:46:58 -0700

    worked on email validation

--- main.js
+++ main.js
@@ -7,20 +7,36 @@ document.addEventListener("DOMContentLoaded", function () {
   inputEmail.addEventListener("submit", function (e) {
     e.preventDefault();
 
-    const t = document.getElementById("input");
-    t.value.length ? (alert("Thanks for subscribing!"),
-      t.value = "") : alert("Please submit a valid email address.")
+    const text = document.getElementById("input");
+    const trimVal = text.value.trim();
+    if (trimVal == "") {
+      alert("Please submit a valid email address.");
+    }
+
+    else if (trimVal.length >= 0) {
+      alert("Thanks for subscribing!");
+    }
+
+    text.value = "";
 
   });
 
 
-    // Flickity carousel 
-    const elem = document.querySelector('.main-carousel');
-    const flkty = new Flickity(elem, {
-      // options
-      cellAlign: 'left',
-      wrapAround: true,
-      freeScroll: true
-    });
+  // Flickity carousel 
+  const elem = document.querySelector('.main-carousel');
+  const flkty = new Flickity(elem, {
+    // options
+    prevNextButtons: false,
+    autoPlay: true,
+    cellAlign: 'left',
+    wrapAround: true,
+    freeScroll: true
+   
 
-});
+  });
+
+
+ 
+  });
+
+  
